87b8a8097503b4fa254d25d5a3bd812c234e28b1
Ruslan Semenov <ruslan@rsemenov.com>
2013-12-13 15:30:04 +0200

    Add export to x3d, collada, Kerkythea, XAML, jpg

---
+++ Cave.Viewer/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Media.Media3D;
+using Cave.Core;
+using HelixToolkit.Wpf;
+
+namespace Cave.Viewer
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Model3D model;
+        public Model3D Model
+        {
+            get { return model; }
+            set { model = value; RaisePropertyChanged("Model"); }
+        }
+
+        private CaveViewType viewType = CaveViewType.Lines;
+        public CaveViewType ViewType
+        {
+            get { return viewType; }
+            set { viewType = value; CreateModel(); }
+        }
+
+        private CaveGraph caveGraph;
+
+        public MainViewModel()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            caveGraph = CaveGraph.ReadCave(".\\Examples\\cave0.csv");
+            CreateModel();
+        }
+
+        private void CreateModel()
+        {
+            var m = new Model3DGroup();
+            var caveModel = caveGraph.Render(ViewType);
+            foreach (var geometry in caveModel.Geometry)
+            {
+                m.Children.Add(new GeometryModel3D(geometry.Model, Materials.Brown){BackMaterial = Materials.Blue});
+            }
+            Model = m;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string property)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
--- Cave.Viewer/MainWindow.xaml.cs
+++ Cave.Viewer/MainWindow.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Threading;
-using System.Windows;
-using System.Windows.Media.Media3D;
-using HelixToolkit.Wpf;
+﻿using System.Windows;
 using Cave.Core;
+using Microsoft.Win32;
 
 namespace Cave.Viewer
 {
@@ -34,124 +29,20 @@ namespace Cave.Viewer
         {
             ((MainViewModel)DataContext).ViewType = CaveViewType.Tubes;
         }
-    }
 
-    public class MainViewModel : INotifyPropertyChanged
-    {
-        private Model3D model;
-        public Model3D Model
+        private void ExportMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            get { return model; }
-            set { model = value; RaisePropertyChanged("Model"); }
-        }
+            var saveFileDialog1 = new SaveFileDialog();
 
-        private CaveViewType viewType = CaveViewType.Lines;
-        public CaveViewType ViewType
-        {
-            get { return viewType; }
-            set { viewType = value; CreateModel(); }
-        }
+            saveFileDialog1.Filter = "Image (*.jpg)|*.jpg|X3D (*.x3d)|*.x3d|Kerkythea (*.xml)|*.xml|Collada (*.dae)|*.dae|XAML (*.xaml)|*.xaml";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
 
-        private CaveGraph caveGraph;
-
-        public MainViewModel()
-        {
-            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-            caveGraph = CaveGraph.ReadCave(".\\Examples\\cave0.csv");
-            CreateModel();
-        }
-
-        private void CreateModel()
-        {
-            var m = new Model3DGroup();
-            var caveModel = caveGraph.Render(ViewType);
-            foreach (var geometry in caveModel.Geometry)
+            if (saveFileDialog1.ShowDialog() == true)
             {
-                m.Children.Add(new GeometryModel3D(geometry.Model, Materials.Brown){BackMaterial = Materials.Blue});
+                var file = saveFileDialog1.FileName;
+                MainViewport3D.Export(file);
             }
-            Model = m;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChanged(string property)
-        {
-            var handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(property));
-            }
-        }
-    }
-
-    public class PanelModelBuilder
-    {
-        public List<Panel> Panels { get; set; }
-
-        public class Panel
-        {
-            public Point3D[] Points { get; set; }
-            public int TriangleIndex { get; set; }
-        }
-
-        public List<int> TriangleIndexToPanelIndex { get; set; }
-
-        public PanelModelBuilder()
-        {
-            Panels = new List<Panel>();
-        }
-
-        public void AddPanel(params Point3D[] points)
-        {
-            Panels.Add(new Panel { Points = points });
-        }
-
-        public void AddPanel(params double[] coords)
-        {
-            Point3D[] points = new Point3D[coords.Length / 3];
-            for (int i = 0; i < coords.Length / 3; i++) points[i] = new Point3D(coords[i * 3], coords[i * 3 + 1], coords[i * 3 + 2]);
-            AddPanel(points);
-        }
-
-        public Model3D ToModel3D()
-        {
-            var m = new Model3DGroup();
-
-            TriangleIndexToPanelIndex = new List<int>();
-
-            // Add the triangles
-            var tm = new MeshBuilder(false, false);
-            int panelIndex = 0;
-            foreach (var p in Panels)
-            {
-                p.TriangleIndex = tm.Positions.Count;
-                tm.AddTriangleFan(p.Points);
-                for (int i = 0; i < p.Points.Length - 2; i++) TriangleIndexToPanelIndex.Add(panelIndex);
-                panelIndex++;
-            }
-            var panelsGeometry = tm.ToMesh();
-            m.Children.Add(new GeometryModel3D(panelsGeometry, Materials.Red) { BackMaterial = Materials.Blue });
-
-            // Add the nodes
-            var gm = new MeshBuilder();
-            foreach (var p in panelsGeometry.Positions)
-            {
-                gm.AddSphere(p, 0.05);
-            }
-            m.Children.Add(new GeometryModel3D(gm.ToMesh(), Materials.Gold));
-
-            // Add the edges
-            var em = new MeshBuilder();
-            foreach (var p in Panels)
-            {
-                for (int i = 0; i < p.Points.Length; i += 1)
-                {
-                    em.AddCylinder(p.Points[i], p.Points[(i + 1) % p.Points.Length], 0.05, 10);
-                }
-            }
-            m.Children.Add(new GeometryModel3D(em.ToMesh(), Materials.Gray));
-
-            return m;
         }
     }
 }
